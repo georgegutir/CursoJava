3ab412b7e4a3b80eb21c941560361ba9eac93abf
JORGE <JORGE@LAPTOP-5V5S80CS>
2020-12-21 08:22:35 +0100

    unir a bbdd, jdbc

---
+++ ejemplojdbc/src/main/java/formacion/ejemplojdbc/App.java
@@ -0,0 +1,13 @@
+package formacion.ejemplojdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
