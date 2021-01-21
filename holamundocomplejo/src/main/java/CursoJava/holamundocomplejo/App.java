35f11c2e298953f0aed2db9a64a4fc562192e4cf
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-01-21 13:05:41 +0100

    inicio holamundocomplejo

---
+++ holamundocomplejo/src/main/java/CursoJava/holamundocomplejo/App.java
@@ -0,0 +1,13 @@
+package CursoJava.holamundocomplejo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
