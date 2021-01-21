ef539d0beb1576fb695cff96ad6bd984c37801aa
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-01-21 13:56:18 +0100

    Inicio holamundocomplejospring

---
+++ holamundocomplejospring/src/test/java/cursojava/spring/holamundocomplejospring/HolamundocomplejospringApplicationTests.java
@@ -0,0 +1,13 @@
+package cursojava.spring.holamundocomplejospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundocomplejospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
