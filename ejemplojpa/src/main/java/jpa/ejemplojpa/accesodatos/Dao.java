0bdcbd87afdd49fc57f3c5c33b27b92ae5d2bec9
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-01-27 13:20:59 +0100

    DAO con JPA completo

---
+++ ejemplojpa/src/main/java/jpa/ejemplojpa/accesodatos/Dao.java
@@ -0,0 +1,11 @@
+package jpa.ejemplojpa.accesodatos;
+
+public interface Dao<T> {
+	Iterable<T> obtenerTodos();
+	T obtenerPorId(Long id);
+
+	T agregar(T objeto);
+	T modificar(T objeto);
+	void borrar(Long id);
+
+}
---
+++ ejemplojpa/src/main/java/jpa/ejemplojpa/entidades/Cliente.java
@@ -0,0 +1,25 @@
+package jpa.ejemplojpa.entidades;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="clientes")
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre, apellidos, cif;
+	private LocalDate fechaNacimiento;
+	
+}
