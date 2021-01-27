77dc77e6a6ddec97ce7ed393c2b55076aeaecf2f
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-01-27 13:49:45 +0100

    Inicio proyecto actividad2

---
+++ UF2177actividad2/src/main/java/euromillon/Actividad2Application.java
@@ -0,0 +1,13 @@
+package euromillon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad2Application.class, args);
+	}
+
+}
---
+++ UF2177actividad2/src/test/java/UF2177/Actividad2ApplicationTests.java
@@ -0,0 +1,13 @@
+package UF2177;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
