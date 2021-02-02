4e9c1e24205a2164da962a375d365886f5600f57
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-02-02 12:01:49 +0100

    Inicio SpringDataJPA

---
+++ ejemplospringdatajpa/src/main/java/spring/ejemplospringdatajpa/EjemplospringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package spring.ejemplospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplospringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplospringdatajpaApplication.class, args);
+	}
+
+}
