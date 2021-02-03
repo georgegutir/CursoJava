e6125041ebdeb993caaffe5bb30ae0b761ad866d
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-02-03 14:22:36 +0100

    Mejorar lectura código js

--- servidorrestservlets/WebContent/js/cliente.js
+++ servidorrestservlets/WebContent/js/cliente.js
@@ -5,43 +5,45 @@ var idModificar = null;
 $(function() { //Al cargar el documento HTML completo...
 	listar();
 
-	$('form').on('submit', function(e) {
-		e.preventDefault();
-
-		var nombre = $('#nombre').val();
-
-		var fecha = $('#fecha').val();
-
-		var partesFecha = fecha.split('-');
-
-		var fechaNacimiento = { year: partesFecha[0], month: partesFecha[1], day: partesFecha[2] };
-
-		var cliente = { nombre, fechaNacimiento };
-
-		console.log(fecha, partesFecha, fechaNacimiento, cliente);
-
-		if(idModificar) {
-			cliente.id = idModificar;
-
-			$.ajax(url + idModificar, {
-				method: 'PUT',
-				data: JSON.stringify(cliente)
-			}).then(listar);
-
-			id = null;
-
-			$('#nombre').val('');
-			$('#fecha').val('');			
-		} else {
-			$.ajax(url, {
-				method: 'POST',
-				data: JSON.stringify(cliente)
-			}).then(listar);
-
-		}
-
-	});
+	$('form').on('submit', aceptarFormulario);
 });
+
+function aceptarFormulario(e) {
+	e.preventDefault();
+
+	var nombre = $('#nombre').val();
+
+	var fecha = $('#fecha').val();
+
+	var partesFecha = fecha.split('-');
+
+	var fechaNacimiento = { year: partesFecha[0], month: partesFecha[1], day: partesFecha[2] };
+
+	var cliente = { nombre, fechaNacimiento };
+
+	console.log(fecha, partesFecha, fechaNacimiento, cliente);
+
+	if (idModificar) {
+		cliente.id = idModificar;
+
+		$.ajax(url + idModificar, {
+			method: 'PUT',
+			data: JSON.stringify(cliente)
+		}).then(listar);
+
+		id = null;
+
+		$('#nombre').val('');
+		$('#fecha').val('');
+	} else {
+		$.ajax(url, {
+			method: 'POST',
+			data: JSON.stringify(cliente)
+		}).then(listar);
+
+	}
+
+}
 
 function listar() {
 	$.getJSON(url, function(clientes) { //Lanzamos una petición REST de tipo GET a la url /api/clientes
@@ -53,9 +55,9 @@ function listar() {
 			var fechaNacimiento = this.fechaNacimiento ? this.fechaNacimiento.year + '-' + this.fechaNacimiento.month + '-' + this.fechaNacimiento.day : '';
 
 			$('<tr><th>' + this.id + '</th><td>' + this.nombre + '</td><td>' 
-			+ fechaNacimiento
-			+ '</td><td><a href="javascript:borrar(' + this.id + ')">Borrar</a> ' 
-			+ '<a href="javascript:editar(' + this.id + ')">Editar</a></td></tr>').appendTo($('tbody')); //Lo colocamos como filas dentro del tbody
+				+ fechaNacimiento
+				+ '</td><td><a href="javascript:borrar(' + this.id + ')">Borrar</a> ' 
+				+ '<a href="javascript:editar(' + this.id + ')">Editar</a></td></tr>').appendTo($('tbody')); //Lo colocamos como filas dentro del tbody
 		});
 	});
 }
