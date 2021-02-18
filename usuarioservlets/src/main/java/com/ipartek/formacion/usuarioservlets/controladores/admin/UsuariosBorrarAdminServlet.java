9f26b8ae9bde3565255aecc66132d3fe797d7a44
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-02-18 13:09:55 +0100

    Fábrica abstracta de DAO

--- usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/controladores/admin/UsuariosBorrarAdminServlet.java
+++ usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/controladores/admin/UsuariosBorrarAdminServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ipartek.formacion.usuarioservlets.accesodatos.UsuarioDao;
-import com.ipartek.formacion.usuarioservlets.accesodatos.UsuarioDaoMySql;
+import com.ipartek.formacion.usuarioservlets.controladores.Configuracion;
 
 @WebServlet("/admin/borrar")
 public class UsuariosBorrarAdminServlet extends HttpServlet {
@@ -20,7 +20,7 @@ public class UsuariosBorrarAdminServlet extends HttpServlet {
 
 		Long i = Long.parseLong(id);
 
-		UsuarioDao dao = new UsuarioDaoMySql();
+		UsuarioDao dao = Configuracion.daoUsuario;
 
 		dao.borrar(i);
 
