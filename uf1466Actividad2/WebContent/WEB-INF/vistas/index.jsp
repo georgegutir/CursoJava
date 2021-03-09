80311fc2afc74188f2b7b14ec0c6d3a72d0fd3f9
JORGE <JORGE@LAPTOP-5V5S80CS>
2021-03-09 13:03:01 +0100

    solucionado selección y restauración

--- uf1466Actividad2/WebContent/WEB-INF/vistas/index.jsp
+++ uf1466Actividad2/WebContent/WEB-INF/vistas/index.jsp
@@ -1,7 +1,10 @@
 <%@ include file="/WEB-INF/vistas/includes/cabecera.jsp" %>
 
 <h2>Restaurar bbdd</h2>
-<form action="restauracion" method="get">
+<form action="restauracion" method="post" enctype="multipart/form-data">
+	<div class="form-group">
+    	<input type="file" class="form-control-file" id="archivo" name="archivo">
+  	</div>
 	<button type="submit" name="restauracion" value="restauracion" class="btn btn-primary btn-lg">Restaurar bbdd</button>
 </form>
 
--- uf1466Actividad2/src/com/ipartek/formacion/uf1466Actividad2/controladores/RestauracionServlet.java
+++ uf1466Actividad2/src/com/ipartek/formacion/uf1466Actividad2/controladores/RestauracionServlet.java
@@ -1,42 +1,63 @@
 package com.ipartek.formacion.uf1466Actividad2.controladores;
 
+import java.io.File;
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 @WebServlet("/restauracion")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+maxFileSize = 1024 * 1024 * 5, 
+maxRequestSize = 1024 * 1024 * 5 * 5)
 public class RestauracionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String UPLOAD_DIRECTORY = "database_backup";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Iniciando restauración");
-		String usuario = "root";
-		String password = "";
-		String archivo ="C:/Users/JORGE/UF1466_2.sql";
-		String[] restoreCmd = new String[] { "mysql ", "--user=" + usuario, "--password=" + password, "-e",
-				"source " + archivo };
-
-		 Process runProcess = Runtime.getRuntime().exec(restoreCmd);
-		 int processComplete = 0;
-		try {
-			processComplete = runProcess.waitFor();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		 
-		 if(processComplete == 0) {
-			 System.out.println("Restaurado con éxito");
-			 request.getRequestDispatcher("/WEB-INF/vistas/menu.jsp").forward(request, response);
-		 } else {
-			 System.out.println("Ha habido algún error");
-		 }
+		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
+		request.setCharacterEncoding("utf-8");
+		String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+		File uploadDir = new File(uploadPath);
+		if (!uploadDir.exists()) uploadDir.mkdir();
+		
+		String archivo = null;
+		
+		for (Part part : request.getParts()) {
+		    archivo = part.getSubmittedFileName();
+		    
+		    if(archivo != null && archivo.trim().length() > 0) {
+			    part.write(uploadPath + File.separator + archivo);
+			    break;
+		    }
+		}
+		
+		String restoreCmd = "mysql -uroot -e \"source " + uploadPath + File.separator + archivo + "\"";
+		System.out.println(archivo);
+		System.out.println(restoreCmd);
+		try {
+			Process runProcess = Runtime.getRuntime().exec(restoreCmd);
+			int processComplete = runProcess.waitFor();
+			if(processComplete == 0) {
+				System.out.println("Restaurado correctamente");
+				request.getRequestDispatcher("/WEB-INF/vistas/menu.jsp").forward(request, response);
+			}else {
+				System.out.println("Ha habido algún error");
+				doGet(request, response);
+			}
+		} catch (InterruptedException e) {		
+			System.out.println("Proceso no completado");
+			doGet(request, response);
+		}		
 	}
 
 }
